22fc7f83327d734bec0e4e0ace791e1f091c1675
MumbleNGrumble <le.daniel90@gmail.com>
2017-06-17 19:37:15 -0700

    Initial commit

---
+++ Week 2/Exercise/Exercise_Square.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 17 19:35:54 2017
+
+@author: Daniel
+"""
+
+def square(x):
+    '''
+    x: int or float.
+    '''
+    # Your code here
+    return x*x
