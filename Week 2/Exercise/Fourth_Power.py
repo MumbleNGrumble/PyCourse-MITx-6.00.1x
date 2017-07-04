7f5cd5e6446bb9119370c4d05b9558f46cf2e1a1
MumbleNGrumble <le.daniel90@gmail.com>
2017-07-04 02:29:11 -0700

    Initial commit

---
+++ Week 2/Exercise/Fourth_Power.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 02:26:23 2017
+
+@author: Daniel
+"""
+
+def square(x):
+    '''
+    x: int or float.
+    '''
+    # Your code here
+    
+    return x*x
+    
+def fourthPower(x):
+    '''
+    x: int or float.
+    '''
+    # Your code here
+    
+    return square(x)*square(x)
