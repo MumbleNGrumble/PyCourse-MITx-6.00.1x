9c34a893053519dad99ba6e308fbf3ffbe2e0b2a
MumbleNGrumble <le.daniel90@gmail.com>
2017-07-04 02:32:51 -0700

    Initial commit

---
+++ Week 2/Exercise/Odd.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 02:30:13 2017
+
+@author: Daniel
+"""
+
+def odd(x):
+    '''
+    x: int
+
+    returns: True if x is odd, False otherwise
+    '''
+    # Your code here
+    
+    return x % 2 == 1
