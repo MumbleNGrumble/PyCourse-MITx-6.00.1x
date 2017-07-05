15d3a2ffa9627f6905acd13ac200786a292a1307
MumbleNGrumble <le.daniel90@gmail.com>
2017-07-04 18:26:59 -0700

    Initial commit

---
+++ Week 1/Exercise/While.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 18:24:35 2017
+
+@author: Daniel
+"""
+
+def whileExercise1():
+    num = 0
+    
+    while num < 10:
+        num += 2
+        print(num)
+    
+    print("Goodbye!")
+
+def whileExercise2():
+    print("Hello!")
+    
+    num = 10
+    
+    while num > 0:
+        print(num)
+        num -= 2
+
+def whileExercise3():
+    i = 0
+    ans = 0
+    
+    while i <= end:
+        ans += i
+        i += 1
+    
+    print(ans)
