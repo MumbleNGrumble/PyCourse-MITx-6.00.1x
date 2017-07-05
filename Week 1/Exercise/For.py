3f91d306a9662dbbcfc1fd01d1381871956e963f
MumbleNGrumble <le.daniel90@gmail.com>
2017-07-04 18:30:29 -0700

    Initial commit

---
+++ Week 1/Exercise/For.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 18:27:12 2017
+
+@author: Daniel
+"""
+
+def forExercise1():
+    for num in range(2,11,2):
+        print(num)
+    
+    print("Goodbye!")
+
+def forExercise2():
+    print("Hello!")
+    
+    for num in range(10,0,-2):
+        print(num)
+
+def forExercise3():
+    ans = 0
+    
+    for num in range(1,(end + 1)):
+        ans += num
+    
+    print(ans)
